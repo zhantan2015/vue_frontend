6cef731830996349bd12ab735a54c252f7c61707
zhantan <zhantan2015@gmail.com>
2022-07-29 12:58:43 +0800

    push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="home">
-    <Article></Article>
+    <Article class="article" v-for="article in articleList" :key="article.aid" :article="article"></Article>
   </div>
 </template>
 
 <script setup>
 // @ is an alias to /src
-import Article from '@/components/articleComponent.vue'
-const props = defineProps({
-  dark: Boolean
-})
+import Article from '@/components/ArticleComponent.vue'
+// import { reactive } from 'vue';
+import { storeToRefs } from 'pinia'
+import { articleState } from '@/store';
+
+
+
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+
+const articleStateObj = articleState()
+
+const { articleList } = storeToRefs(articleStateObj)
 </script>
+<style lang="less">
+.article {
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #7f8c8d;
+
+  &:last-child {
+    border: none;
+  }
+}
+</style>
