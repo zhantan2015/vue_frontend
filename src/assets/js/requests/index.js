5eb870f33e32a9c3be9b0420c0024a2f3fb8a8e8
zhantan <zhantan2015@gmail.com>
2022-07-29 16:08:42 +0800

    push-fixed

--- src/assets/js/requests/index.js
+++ src/assets/js/requests/index.js
@@ -6,7 +6,7 @@ function getToken() {
 }
 
 const service = axios.create({
-    baseURL: process.env.REQUESTS_BASE_URL || '//localhost:8888',
+    baseURL: process.env.VUE_APP_REQUESTS_BASE_URL || '//localhost:8888',
     timeout: 60 * 1000,
 })
 
