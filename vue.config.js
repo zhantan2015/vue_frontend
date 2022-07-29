da0def8d1ad0615b5eabfa89bc6e917cc55fc808
zhantan <zhantan2015@gmail.com>
2022-07-29 22:03:22 +0800

    更改PWA名称

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+
+  pwa: {
+    name: '川之上'
+  }
+
 })
